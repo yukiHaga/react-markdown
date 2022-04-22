91ba0c53f9de37d867ac0fcfbfac0b091a26c1dc
yukiHaga <oceansthirteen7510@gmail.com>
2022-04-22 23:25:23 +0900

    Add: buttonにスタイルを追加した

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,6 +1,10 @@
 import { ReactNode } from "react";
+import { propTypes } from "react-markdown";
 import styled from "styled-components";
 
+// &.はscssの構文。scssの構文をstyled-componentsでも使える
+// StyledButton.cancelというcssセレクタにコンパイルされる
+// cancelが存在するときだけ適用される
 const StyledButton = styled.button`
   background-color: dodgerblue;
   border: none;
@@ -10,13 +14,22 @@ const StyledButton = styled.button`
   height: 2rem;
   min-width: 5rem;
   padding: 0 1rem;
+  &.cancel {
+    background: white;
+    border: 1px solid gray;
+    color: gray;
+  }
 `;
 
+// ?はオブジェクトのプロパティがあってもなくても良いを表す。
 type Props = {
   onClick: () => void;
   children: ReactNode;
+  cancel?: boolean;
 };
 
-export const Button = ({ onClick, children }: Props): JSX.Element => (
-  <StyledButton onClick={onClick}>{children}</StyledButton>
+export const Button = ({ onClick, children, cancel }: Props): JSX.Element => (
+  <StyledButton onClick={onClick} className={cancel ? "cancel" : ""}>
+    {children}
+  </StyledButton>
 );
