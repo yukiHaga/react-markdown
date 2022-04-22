884d29e01900ac503d306e4e6c0f873cbfa4dd6f
yukiHaga <oceansthirteen7510@gmail.com>
2022-04-22 19:39:30 +0900

    Add: グローバルスタイルとエディターページを作成した

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,28 @@
 import * as React from "react";
 import { render } from "react-dom";
-import styled from "styled-components";
+import { Editor } from "./pages/editor";
+import { createGlobalStyle } from "styled-components";
 
-const Header = styled.h1`
-  color: red;
+// styled-componentsのcreateGlobalStyleを使って、
+// ページ全体に適用できるスタイルを定義しています。
+// widthとheightは初期状態だと、コンテンツに対して指定される
+// 以下のように設定することで、widthとheightにpaddingとborderが含まれる
+// widthとheightが固定されている場合、
+// paddingとborderがでかくなるごとに、中のcontentが小さくなる。
+// 「*」は、すべての要素にスタイルを適用する全称セレクタです
+const GlobalStyle = createGlobalStyle`
+  body * {
+    box-sizing: border-box;
+  }
 `;
 
-const Main = <Header>最強のマークダウンエディタ</Header>;
+// styled-componentsのグローバルスタイルを適用させるためには、
+// createGlobalStyleで作ったGlobalStyleを、
+// リアクトツリーの最上位のコンポーネントの中にGlobalStyleを書く
+const Main = (
+  <>
+    <GlobalStyle />
+    <Editor />
+  </>
+);
 render(Main, document.getElementById("app"));
