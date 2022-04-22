0a519a7523ca85b7781f12546af4d80ab78a6af5
yukiHaga <oceansthirteen7510@gmail.com>
2022-04-23 03:04:53 +0900

    Add: 一定のデータ量のみ表示するように設定した

--- src/indexeddb/memos.ts
+++ src/indexeddb/memos.ts
@@ -32,7 +32,27 @@ export const putMemo = async (title: string, text: string): Promise<void> => {
   await memos.put({ datetime, title, text });
 };
 
+const NUM_PER_PAGE: number = 10;
+
+// memos.count()で、オブジェクトストアから総件数を取得します
+// count()はDexieに定義された関数です
+// Math.ceil()は小数点以下を切り上げた数値を戻り値として返します
+// 四捨五入とか関係なしに切り上げます
+export const getMemoPageCount = async (): Promise<number> => {
+  const totalCount = await memos.count();
+  const pageCount = Math.ceil(totalCount / NUM_PER_PAGE);
+  return pageCount > 0 ? pageCount : 1;
+};
+
 // toArrayで取得したデータを配列にしている
-export const getMemos = (): Promise<MemoRecord[]> => {
-  return memos.orderBy("datetime").reverse().toArray();
+// offsetは位置を表す
+// offset()は取得するリスト内の開始位置を設定します。
+export const getMemos = (page: number): Promise<MemoRecord[]> => {
+  const offset = (page - 1) * NUM_PER_PAGE;
+  return memos
+    .orderBy("datetime")
+    .reverse()
+    .offset(offset)
+    .limit(NUM_PER_PAGE)
+    .toArray();
 };
--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Header } from "../components/header";
-import { getMemos, MemoRecord } from "../indexeddb/memos";
+import { getMemoPageCount, getMemos, MemoRecord } from "../indexeddb/memos";
 
 const HeaderArea = styled.div`
   position: fixed;
@@ -12,12 +12,13 @@ const HeaderArea = styled.div`
 `;
 
 const Wrapper = styled.div`
-  bottom: 0;
+  bottom: 3rem;
   left: 0;
   position: fixed;
   right: 0;
   top: 3rem;
   padding: 0 1rem;
+  overflow-y: scroll;
 `;
 
 const Memo = styled.button`
@@ -28,6 +29,29 @@ const Memo = styled.button`
   padding: 1rem;
   margin: 1rem 0;
   text-align: left;
+`;
+
+const Paging = styled.div`
+  bottom: 0;
+  height: 3rem;
+  left: 0;
+  line-height: 2rem;
+  padding: 0.5rem;
+  position: fixed;
+  right: 0;
+  text-align: center;
+`;
+
+const PagingButton = styled.button`
+  background: none;
+  border: none;
+  display: inline-block;
+  height: 2rem;
+  padding: 0.5rem 1rem;
+
+  &:disabled {
+    color: silver;
+  }
 `;
 
 const MemoTitle = styled.div`
@@ -48,10 +72,25 @@ type Props = {
 
 export const History = ({ setText }: Props): JSX.Element => {
   const [memos, setMemos] = useState<MemoRecord[]>([]);
+  const [page, setPage] = useState(1);
+  const [maxPage, setMaxPage] = useState(1);
   const navigate = useNavigate();
+
   useEffect(() => {
-    getMemos().then(setMemos);
+    getMemos(1).then(setMemos);
+    getMemoPageCount().then(setMaxPage);
   }, []);
+
+  const canNextPage: boolean = page < maxPage;
+  const canPrevPage: boolean = page > 1;
+  const movePage = (targetPage: number) => {
+    if (targetPage < 1 || maxPage < targetPage) {
+      return;
+    }
+    setPage(targetPage);
+    getMemos(targetPage).then(setMemos);
+  };
+
   return (
     <>
       <HeaderArea>
@@ -74,6 +113,21 @@ export const History = ({ setText }: Props): JSX.Element => {
           </Memo>
         ))}
       </Wrapper>
+      <Paging>
+        <PagingButton
+          onClick={() => movePage(page - 1)}
+          disabled={!canPrevPage}
+        >
+          ＜
+        </PagingButton>
+        {page} / {maxPage}
+        <PagingButton
+          onClick={() => movePage(page + 1)}
+          disabled={!canNextPage}
+        >
+          ＞
+        </PagingButton>
+      </Paging>
     </>
   );
 };
